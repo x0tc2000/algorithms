cf66e7e9cda3526ecc8615c6eb2ad777770f8a1d
Roman Belousov <x0tc2000@gmail.com>
2021-01-26 22:59:52 +0300

    217. Contains Duplicate (2 solution)

--- first-100/217.js
+++ first-100/217.js
@@ -3,4 +3,18 @@
 
 const containsDuplicate = (nums) => new Set(nums).size !== nums.length
 
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+// const containsDuplicate = (nums) => {
+//   const dict = new Map()
+
+//   for (const num of nums) {
+//     if (dict.has(num)) return false
+//     dict.set(num, 1)
+//   }
+
+//   return false
+// }
+
 console.log(containsDuplicate([1, 2, 3, 1])) //  true
